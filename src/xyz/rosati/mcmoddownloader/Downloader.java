1aba10537b17c31dfc74fb3750cd15564c788fc3
Nicholas Rosati <nicholas@rosti.xyz>
2019-12-03 23:57:09 -0500

    JavaDocs

--- src/xyz/rosati/mcmoddownloader/Downloader.java
+++ src/xyz/rosati/mcmoddownloader/Downloader.java
@@ -14,27 +14,40 @@ public class Downloader {
         this.converter = new Converter();
     }
 
-    public void download(String projectFileURL, String destFilePath) throws Exception {
+    /**
+     * Downloads a mod from CurseForge
+     * @param projectFileURL The URL to a version of a mod.<br>Example: https://curseforge.com/minecraft/mc-mods/[mod name]/[files]/[file id]
+     * @param destFilePath The path to where the mod file will be stored.
+     * @throws Exception Thrown if invalid URL.
+     * @throws IOException Thrown if network error when downloading the mod.
+     */
+    public void download(String projectFileURL, String destFilePath) throws Exception, IOException {
         String filename;
         if (!validateProjectFileURL(projectFileURL)) { throw new Exception("Invalid URL was given."); }
 
-        try {
-            filename = getFileName(projectFileURL);
-        } catch (IOException e) {
-            throw new Exception("Error downloading site HTML.\n" + e);
-        }
+        filename = getFileName(projectFileURL);
 
         System.out.println("filename: " + filename);
     }
 
+    /**
+     * Ensures the given project file URL is valid
+     * @param url A URL that points to a version of a mod.<br>Example: https://curseforge.com/minecraft/mc-mods/[mod name]/[files]/[file id]
+     * @return True if valid, false if invalid
+     */
     private boolean validateProjectFileURL(String url) {
-        //File URL in the format https://curseforge.com/minecraft/mc-mods/[mod name]/[files]/[file id]
         System.out.println(url);
         return Pattern.matches("^https://(www\\.)?curseforge\\.com/minecraft/mc-mods/[A-Za-z0-9_-]+/files/[0-9]+", url);
     }
 
+    /**
+     * Gets the file name for a given CurseForge project file URL
+     * @param projectFileURL A URL pointing to a specific version of a mod
+     * @return A string to the file name associated with the given project mod version
+     * @throws IOException Thrown if errors occurs when sending GET request
+     */
     private String getFileName(String projectFileURL) throws IOException {
-        //Put the header information into a hashmap for use when connecting to CurseForge
+        //Put the header information into a HashMap for use when connecting to CurseForge
         HashMap<String, String> header = new HashMap<>();
         header.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
         header.put("Accept-Language", "en-US,en;q=0.5");
@@ -43,7 +56,7 @@ public class Downloader {
         header.put("Cache-Control", "no-cache");
         //Get HTML of the mod version's project page
         Document doc = Jsoup.connect(projectFileURL).headers(header).get();
-
+        //Select the class containing the file name and return it
         return doc.select("h3[class=\"text-primary-500 text-lg\"]").first().text();
     }
 }
