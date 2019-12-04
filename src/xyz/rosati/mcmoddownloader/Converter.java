8e881143c0f5aba4450f97f4612c1e59e496ced0
Nicholas Rosati <nicholas@rosti.xyz>
2019-12-03 23:57:45 -0500

    Add CurseForge to ForgeCDN converter

--- src/xyz/rosati/mcmoddownloader/Converter.java
+++ src/xyz/rosati/mcmoddownloader/Converter.java
@@ -13,17 +13,24 @@ public class Converter {
      * @param originalURL The CurseForge file URL
      * @return The ForgeCDN link to the inputted file.
      */
-    public String CFtoFCDN(String originalURL) throws Exception{
-        StringBuilder convertedURL = new StringBuilder();
+    public String CFtoFCDN(String originalURL, String filename) throws Exception{
         final String REGEX = "^https:\\/\\/(www\\.)?curseforge\\.com\\/minecraft\\/mc-mods\\/[A-Za-z0-9_-]+\\/files\\/[0-9]+";
-        boolean validURL = Pattern.matches(REGEX, originalURL);
+        //Test if the given file URL is valid
+        if (!Pattern.matches(REGEX, originalURL)) { throw new Exception("URL not valid."); }
 
-        if (!validURL) { throw new Exception("URL not valid."); }
-
-
-
-
+        StringBuilder convertedURL = new StringBuilder();
         convertedURL.append(FORGECDN_BASE);
+
+        //TODO: Make this cleaner. It works but it feels hacky
+        char[] fileID = {0,0,0,0,0,0,0,0,0,0,0,0};
+        originalURL.getChars(originalURL.lastIndexOf('/') + 1, originalURL.length(), fileID, 0);
+        for (int i = 0; i < 7; ++i) {
+            if (i == 4) convertedURL.append('/');
+            convertedURL.append(fileID[i]);
+        }
+
+        convertedURL.append('/');
+        convertedURL.append(filename);
 
         return convertedURL.toString();
     }
