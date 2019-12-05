81e2dfd88a5d49eb9b4be4112862b68de8a29d83
Nicholas Rosati <nicholas@rosti.xyz>
2019-12-04 21:12:20 -0500

    Readability improvements to FCDN converter

--- src/xyz/rosati/mcmoddownloader/Converter.java
+++ src/xyz/rosati/mcmoddownloader/Converter.java
@@ -16,17 +16,11 @@ public final class Converter {
         //Test if the given file URL is valid
         if (!Pattern.matches("^https://(www\\.)?curseforge\\.com/minecraft/mc-mods/[A-Za-z0-9_-]+/files/[0-9]+", originalURL)) { throw new Exception("URL not valid."); }
 
-        StringBuilder convertedURL = new StringBuilder(FORGECDN_BASE);
-
-        //TODO: Make this cleaner. It works but it feels hacky
-        char[] fileID = {0,0,0,0,0,0,0,0};
-        originalURL.getChars(originalURL.lastIndexOf('/') + 1, originalURL.length(), fileID, 0);
-        for (int i = 0; i < 7; ++i) {
-            if (i == 4) convertedURL.append('/');
-            convertedURL.append(fileID[i]);
-        }
-
-        convertedURL.append('/').append(filename);
-        return convertedURL.toString();
+        int fileIDStart = originalURL.lastIndexOf("/") + 1;
+        return FORGECDN_BASE +
+               originalURL.substring(fileIDStart, fileIDStart + 4) +
+               '/' +
+               originalURL.substring(fileIDStart + 4) +
+               '/' + filename;
     }
 }
